ae4c321c796e020bc9c290ec137870a9c3cd7090
GreenChild04 <106739622+GreenChild04@users.noreply.github.com>
2023-08-22 11:22:59 +1000

    added home path

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,11 @@ pub mod list;
 pub mod entry;
 
 pub use logger::*;
+
+pub fn home_dir() -> std::path::PathBuf {
+    // Linux only; change this if you want to go cross platform
+    match std::env::var("HOME") {
+        Ok(path) => std::path::Path::new(&path).join("diary-cli"),
+        Err(_) => std::path::PathBuf::from("/etc/diary-cli/"),
+    }
+}
